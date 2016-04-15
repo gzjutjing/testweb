4856438fff6ba5a88c310e3cf9376215b65c7283
gzjutjing <149264267@qq.com>
2016-04-15 23:48:44 +0800

    validate

--- src/main/java/com/test/domain/TestDomain.java
+++ src/main/java/com/test/domain/TestDomain.java
@@ -1,10 +1,18 @@
 package com.test.domain;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by admin on 2016/4/15.
  */
 public class TestDomain {
+    @NotNull
     private Integer id;
+    @NotNull
+    @Min(value = 2,message = "{name.min.message}")
+    @Max(value = 6,message = "最长长度为6")
     private String name;
 
     public Integer getId() {
